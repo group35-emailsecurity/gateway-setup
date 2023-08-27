51516c821e1766ae92411396a52283acb3006ade
Gregory Ruta <102509965@student.swin.edu.au>
2023-08-27 12:16:44 +1000

    update script

--- postfix/setup.sh
+++ postfix/setup.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # Prompt user for server IP addresses
-read -p "Enter internal server IP address: " INTERNAL_IP
-read -p "Enter external server IP address: " EXTERNAL_IP
+read -p "Enter internal server IP address: " INTERNAL_IP &&
+read -p "Enter external server IP address: " EXTERNAL_IP &&
 
 # Check if Postfix is installed
-dpkg --status "postfix" &> /dev/null
+dpkg --status "postfix" &> /dev/null &&
 
 # If Postfix is installed, uninstall it
 if [ $? -eq 0 ]; then
-    apt -y purge postfix
+    apt -y purge postfix &&
 fi
 
 # Check if Opdendkim is installed
-dpkg --status "opendkim" &> /dev/null
+dpkg --status "opendkim" &> /dev/null &&
 
 # If Opdendkim is installed, uninstall it
 if [ $? -eq 0 ]; then
-    apt -y purge opendkim
+    apt -y purge opendkim &&
 fi
 
 # Install packages
@@ -42,27 +42,27 @@ cat main.cf-additional-settings >> /etc/postfix/main.cf &&
 cat master.cf-additional-settings >> /etc/postfix/master.cf &&
 
 # Append additional settings to the opendkim.conf config file
-cat opendkim.conf-additional-settings >> /etc/opendkim.conf
+cat opendkim.conf-additional-settings >> /etc/opendkim.conf &&
 
 # Append additional settings to the opendkim config file
-cat openkdim-default-additional-settings >> /etc/default/opendkim
+cat openkdim-default-additional-settings >> /etc/default/opendkim &&
 
 # Set IP addresses
-sed -i "s/<INTERNAL_SERVER_IP_ADDRESS>/$INTERNAL_IP/g" /etc/postfix/transport-maps /etc/opendkim.conf
-sed -i "s/<EXTERNAL_SERVER_IP_ADDRESS>/$EXTERNAL_IP/g" /etc/postfix/transport-maps
+sed -i "s/<INTERNAL_SERVER_IP_ADDRESS>/$INTERNAL_IP/g" /etc/postfix/transport-maps /etc/opendkim.conf &&
+sed -i "s/<EXTERNAL_SERVER_IP_ADDRESS>/$EXTERNAL_IP/g" /etc/postfix/transport-maps &&
 
 # Generate opendkim key
-opendkim-genkey -t -s mail -d internal.test
+opendkim-genkey -t -s mail -d internal.test &&
 
 # Copy private key to postfix directory
-cp mail.private /etc/postfix/dkim.key
+cp mail.private /etc/postfix/dkim.key &&
 
 # Set the relay maps
 postmap /etc/postfix/transport-maps &&
 postmap /etc/postfix/relay-recipient-maps &&
 
 # Reload Postfix
-postfix reload
+postfix reload &&
 
 # Start opendkim
-service opendkim start
+service opendkim start &&
