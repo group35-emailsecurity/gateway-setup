502214e323c33911c51af12374723818d48c13e4
David Mackenzie <48002743+DavidAustinMackenzie@users.noreply.github.com>
2023-09-01 16:30:55 +1000

    Added originalEmail property to Email class

--- postfix/filter-service/models/email.py
+++ postfix/filter-service/models/email.py
@@ -4,10 +4,12 @@ class Email:
     fromAddress = ""
     subject = ""
     body = ""
+    originalEmail = ""
 
-    def __init__(self, id, toAddress, fromAddress, subject, body ):
+    def __init__(self, id, toAddress, fromAddress, subject, body, originalEmail):
         self.id = id
         self.toAddress = toAddress
         self.fromAddress = fromAddress
         self.subject = subject
         self.body = body
+        self.originalEmail = originalEmail
