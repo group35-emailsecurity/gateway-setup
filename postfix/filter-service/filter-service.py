ec37be838c96a18b21792ce9df7d71e431d58932
Gregory Ruta <102509965@student.swin.edu.au>
2023-09-03 12:15:43 +1000

    Tidy filter-service.py

--- postfix/filter-service/filter-service.py
+++ postfix/filter-service/filter-service.py
@@ -2,7 +2,6 @@
 import email
 import re
 import sys
-import os
 import re
 import utilities
 from models.email import Email
@@ -17,48 +16,41 @@ from email import policy
 # Exit code 0: No threat detected. This exit code will result in the email being delivered to the intended recipient.
 # Exit code 1: Threat detected. This exit code will result in the email being discarded.
 
-
 class Outcome(Enum):
     ALLOWED = 0
     DENIED = 1
 
-
+# Raw email in string format
 emailStr = sys.stdin.read()
 
 # Email in object form with each element (To, From, Subject, etc.) mapped to an object property.
 emailObj = email.message_from_string(emailStr, policy=email.policy.default)
 
-# Check and print "To" address
-emailToAddress = emailObj['To']
-
-# Check and print "Subject"
-emailSubject = emailObj['Subject']
-
-# Check and extract "From" address using regex
+# Get 'from' address
 fromDetails = emailObj['From']
-emailRegex = r'<([^>]+)>'
-
-match = re.search(emailRegex, fromDetails)
-
+emailFromregex = r'<([^>]+)>'
+match = re.search(emailFromregex, fromDetails)
 emailFromAddress = match.group(1)
 
+# Get other elements
+emailToAddress = emailObj['To']
+emailSubject = emailObj['Subject']
 emailBody = emailObj.get_body(('plain',)).get_content()
 
 exitCode = Outcome.ALLOWED.value
 
-regex = r'@(.*)'
-if re.findall(regex, emailFromAddress)[0] == "internal.test":
-    # Encrypt outgoing emails
+domainRegex = r'@(.*)'
+if re.findall(domainRegex, emailFromAddress)[0] == "internal.test":
+    # Outbound emails logic
+    # Keyword list scanning for outgoing emails
     protectedKeywordList = ["secret", "confidential", "private"]
-    
-    # Protected Keyword scanning
     for keyword in protectedKeywordList:
-        if re.search(keyword, emailStr, re.IGNORECASE):
+        if re.search(keyword, emailBody, re.IGNORECASE):
             # Give the email a new body, overwriting the old one
             newBody = "Encrypted message attached"
             emailObj.set_content(newBody)
 
-            # Pipe oldBody into gpg symmetric, save encrypted.gpg file
+            # Pipe oldBody into gpg and save symetrically encrypted 'encrypted.gpg' file
             filename = "encrypted.gpg"
             gpgPassphrase = "open"
             run(['gpg', '--output', filename, '--symmetric', '--passphrase', gpgPassphrase, '--batch', '--yes'], cwd='/home/user/', input=emailBody, text=True)
@@ -70,14 +62,15 @@ if re.findall(regex, emailFromAddress)[0] == "internal.test":
             # Attach encrypted.gpg to email
             emailObj.add_attachment(data, maintype='application', subtype='octet-stream', filename=filename)
 
-            # Overwrite the original email with the new version
+            # Overwrite the original email with the new version. New version will be piped back into postfix via filter-handler.sh for delivery
             with open('/home/user/temp-email-file.tmp', 'w') as file:
                 file.write(emailObj.as_string())
             
             logMessage = "The word %s was found in the email. Encrypting outgoing email." % keyword
             break
-    
+
 else:
+    #Inbound emails logic
     print("inbound email that needs to be scanned")
     keywordBlacklist = ["casino", "lottery", "viagra"]
     emailOutcome = Outcome.ALLOWED.name
@@ -104,16 +97,16 @@ else:
         logMessage = "Infected attachment detected"
 
     # Create and Add Email record
-    emailCount = utilities.readListCount('../../webapp/data/emails.bin')
+    emailCount = utilities.getEmailListCount('../../webapp/data/emails.bin')
     emailCount += 1
     emailId = emailCount
 
     # Create Email Object
-    emailRecord = Email(emailId, emailToAddress, emailFromAddress, emailSubject, emailBody)
+    emailRecord = Email(emailId, emailToAddress, emailFromAddress, emailSubject, emailBody, emailStr)
 
     # Get current email records and add Email record to list
     emailList = []
-    emailList = utilities.readFromBinaryFileToEmailList('../../webapp/data/emails.bin')
+    emailList = utilities.readFromBinaryFileToEmailList('../../webapp/data/emails.bin', emailList)
     emailList.append(emailRecord)
 
     # Write the updated Email List to bin file
@@ -123,11 +116,11 @@ else:
     # TODO: Add To, From and subject from Email variable
     #################################### Create new Log Record ########################################
     # Create and Add Log record
-    logCount = utilities.readListCount('../../webapp/data/logs.bin')
+    logCount = utilities.getLogListCount('../../webapp/data/logs.bin')
     logCount += 1
     logId = logCount
     logDate = date.today()
-    logTime = time.strftime('%H:%M:%S')
+    logTime = "11:32"
     logTo = "user@group35.com"
     logFrom = "admin@group35.com"
     logSubject = "Please meet in boardroom at 1PM"
@@ -145,6 +138,5 @@ else:
     #################################### Create new Log Record ########################################
     
 print(logMessage)
-
 
 sys.exit(exitCode)
